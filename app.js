c05a4ce1d26c5ea3b043d4c14af1558a0659672b
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-03-19 23:07:11 +0500

    This is for an assigment for SMiT

---
+++ app.js
@@ -0,0 +1,113 @@
+
+// ******* Chapter # 1
+
+// 1
+alert("Hello Guys Welcome to our store");
+
+// 2
+alert("Error! Please Enter a valid password.");
+
+// 3
+alert("Welcome to JS Land...\n Happy Coding!");
+
+// 4
+alert("Welcome to JS LAnd...");
+alert("Happy Coding");
+
+// 5
+//  at  console
+
+// 6
+// in html
+
+// 7
+// in html
+
+// *******chapter # 2
+// 1
+var username;
+
+// 2
+var myName = "Muhammad Talha Noor";
+// 3
+var message = "Hello World";
+alert(message);
+//  4
+var name1 = "Jhone Doe";
+var age = "15 years old";
+var course = "Certified Mobile Application Development";
+alert(name1);
+alert(age);
+alert(course);
+
+// 5
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// 6
+var email = "talhanoormalik0321@gmail.com";
+alert("My email address is " + email);
+
+// 7
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+// 8
+document.write("Yah! I can write HTML content through JavaScript");
+
+// 9
+var contentForNine = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(contentForNine);
+
+// *********chapter # 3
+
+// 1
+var age = 17;
+alert("I am " + age + " years old");
+
+// 2
+
+// 3
+var birthYear = 2007;
+document.write("My birth year is " + birthYear);
+
+// 4
+var visitorName = "Talha Noor ";
+var quantity = 5;
+var productTitle = "T-Shirts (s)";
+document.write(
+  visitorName +
+    "ordered " +
+    quantity +
+    " " +
+    productTitle +
+    " on MyOwn Clothing Store"
+);
+
+// ********* Chapter # 4
+// 1
+var a = 1,
+  b = 2,
+  c = 3;
+// 2
+// Legal Names
+var justin;
+var Imran;
+var input;
+var my_name;
+var user1;
+//  illegal Names
+//  var 2name;
+// var #myinput;
+// var your name;
+// var stud{ent;
+// var !teda;   
+// 3
+document.write("<h1>Rules for naming JS names</h1>");
+document.write(
+  " Variable names can only contain  numbers, alphabets, $ and _. For example : $my_1stVariable <br/> "
+);
+document.write(
+  " \n Variables must begin with a letter, $ or _. For example $name, _name or name <br/>"
+);
+document.write("Variable names are case sensitive <br/>");
+document.write("Variable names should not be JS keywords <br/>");
